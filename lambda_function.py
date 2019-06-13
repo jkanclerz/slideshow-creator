11a84be157335b51ee456b21a031829d9b4ed2ce
Jakub Kanclerz <jakub.kanclerz@gmail.com>
2019-06-13 15:13:58 +0200

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -8,7 +8,7 @@ def lambda_handler(event, context):
     
     print(event)
     if 'Records' in event:
-        handle_create_animation(event['Records'][0]['body'])
+        handle_create_animation(json.loads(event['Records'][0]['body']))
 
     return {
         'statusCode': 200,
